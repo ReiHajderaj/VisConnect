ca1551f93f5fd05bc561fc6cd8e71b1052db596c
ReiHajderaj <127845203+ReiHajderaj@users.noreply.github.com>
2025-05-21 20:28:50 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "UniTalk",
+  title: "VoxTrans",
   description: "A video calling app",
   icons: {
     icon: '/icons/logo.svg'
